7623d50216accaa1ca8633179b563bc3e563e8da
Shane McQuerter <shanemcquerter@gmail.com>
2016-07-28 17:17:24 -0500

    Solve merge errors

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar';
 import Main from './Main';
-import GoogleMap from '../containers/GoogleMap';
 
 export default function App() {
   return (
     <div>
       <NavBar />
       <Main />
-      <GoogleMap />
     </div>
   );
 }
--- client/containers/GoogleMap.js
+++ client/containers/GoogleMap.js
@@ -4,13 +4,17 @@ import React from 'react';
 import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
 
+
 export default (props) => {
   return (
-    <GoogleMapLoader
-      containerElement={ <div style={{height: '100%'}} /> }
-      googleMapElement={
-        <GoogleMap defaultZoom={12} defaultCenter={{lat: 30.2672, lng: 97.7431}} />
-      }
+    <div>
+
+      <GoogleMapLoader
+        containerElement={ <div style={{height: '300px'}} /> }
+        googleMapElement={
+          <GoogleMap defaultZoom={12} defaultCenter={{lat: 30.307182, lng: -97.755996}} />
+        }
       />
+      </div>
   );
 }
