43c33a3eed19605d9a4e69766f4129e3e703a7f0
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-07-28 17:38:16 -0500

    map shows onto ShowList component

--- client/containers/ShowList.js
+++ client/containers/ShowList.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react"
 import {connect} from "react-redux"
-import {bindActionCreators} from 'redux'
-import { fetchShows } from '../actions/shows'
+import {bindActionCreators} from 'redux';
+import { fetchShows } from '../actions/shows';
+import AustinJSON from '../../songkickEventsAustinJSON';
+const austinEvents = AustinJSON().resultsPage.results.event;
 
 class ShowList extends Component {
   constructor(props) {
@@ -14,10 +16,24 @@ class ShowList extends Component {
   }
 
   render() {
-
-    //console.log('shows from store', this.props.shows);
     return (
-      <div>
+      <div className="list-group">
+        {
+
+          //this.props.shows
+          austinEvents
+          .map(show => {
+            return (
+              <div key={show._id} className="list-group-item">
+                <div className="show-name list-group-item-heading"><h4> { show.displayName }</h4></div>            
+                <div className="show-venue"><h6> { show.venue.displayName }</h6></div>
+                <div className="show-date"><h6> { show.start.date }</h6></div>
+                <div className="show-location"><h6> { show.location.city }</h6></div>
+              </div>
+            )
+          })
+          
+        }
       </div>
     )
   }
