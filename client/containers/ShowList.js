c6e6616704d67116b0ab5170ba947c730676f108
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-07-28 20:03:58 -0500

    fix bug preventing ajax results showing

--- client/containers/ShowList.js
+++ client/containers/ShowList.js
@@ -4,30 +4,31 @@ import {connect} from "react-redux"
 
 class ShowList extends Component {
 
-
   render() {
     console.log('shows from store', this.props.shows);
     return (
       <div className="list-group">
       {
-      //  this.props.shows.resultsPage.results.event.map(show => {
-      //   return (
-      //     <div key={show._id} className="list-group-item">
-      //       <div className="show-name list-group-item-heading"><h4> { show.displayName }</h4></div>
-      //       <div className="show-venue"><h6> { show.venue.displayName }</h6></div>
-      //       <div className="show-date"><h6> { show.start.date }</h6></div>
-      //       <div className="show-location"><h6> { show.location.city }</h6></div>
-      //     </div>
-      //   )
-      // })
-     }
+        this.props.shows[0]
+        ? this.props.shows[0].resultsPage.results.event.map(show => {
+          return (
+            <div key={show.id} className="list-group-item">
+              <div className="show-name list-group-item-heading"><h4> { show.displayName }</h4></div>
+              <div className="show-venue"><h6> { show.venue.displayName }</h6></div>
+              <div className="show-date"><h6> { show.start.date }</h6></div>
+              <div className="show-location"><h6> { show.location.city }</h6></div>
+            </div>
+          )
+        })
+        : null
+      }
       </div>
     )
   }
 }
+
 function mapStateToProps(state) {
   return { shows: state.shows}
 }
-
 
 export default connect(mapStateToProps)(ShowList)
