75ed9ca6186fbe6e3f75803b21337f40b1d19569
Christopher Wiles <chriswiles87@gmail.com>
2016-07-29 11:54:00 -0500

    rework googemap.js

---
+++ client/components/GoogleMap.js
@@ -0,0 +1,34 @@
+// Austin Coords: 30.2672° N, 97.7431° W
+import React, {Component} from 'react';
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
+
+export default class DrawMap extends Component {
+// const austinEvents = this.props.shows[0].resultsPage.results.event;
+
+  render() {
+    return (
+      <div>
+        <GoogleMapLoader
+          containerElement={ <div style={{height: '95vh'}} /> }
+          googleMapElement={
+            <GoogleMap defaultZoom={12} defaultCenter={{lat: 30.2747, lng: -97.7404}} >
+              {
+                this.props.shows[0]
+                ? this.props.shows[0].resultsPage.results.event.map( (show, index) => {
+                  return (
+                    <Marker
+                      key={index}
+                      position={{lat: show.venue.lat, lng: show.venue.lng}}
+                    />
+                  );
+                })
+                : null
+              }
+
+            </GoogleMap>
+          }
+        />
+      </div>
+    );
+  }
+}
