baa96cd061f7439e1d0dc129f7636688c1d5bdff
Christopher Wiles <chriswiles87@gmail.com>
2016-07-29 14:48:36 -0500

    add geolocation

--- client/actions/shows.js
+++ client/actions/shows.js
@@ -1,16 +1,7 @@
 import axios from 'axios';
 export const FETCH_SHOWS = 'FETCH_SHOWS';
 
-function getLocation() {
-    console.log('asking for geolocation..')
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => fetchShows(position));
-    } else {
-      console.log("Geolocation is not supported by this browser.")
-    }
-}
-
-export function fetchShows(long,lat) {
+export function fetchShows(geo) {
   //const shows = axios.post("/fetchShows", {long: "-97.7431" , lat: "30.2669444"}).then((data) => console.log("axios.get(/test)", data))
   // redux middleware sends promise
   // return {
@@ -19,6 +10,6 @@ export function fetchShows(long,lat) {
   // }
   return {
     type: FETCH_SHOWS,
-    payload :  axios.post("/fetchShows", {long: "-97.7431" , lat: "30.2669444"})
+    payload :  axios.post("/fetchShows", {long: geo.coords.longitude , lat: geo.coords.latitude})
   }
 }
