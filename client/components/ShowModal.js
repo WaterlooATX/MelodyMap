a05ba77acd324b5633291b942bf0f7a2e50c03e9
Christopher Wiles <chriswiles87@gmail.com>
2016-07-29 08:06:24 -0500

    add model text

--- client/components/ShowModal.js
+++ client/components/ShowModal.js
@@ -1,5 +1,30 @@
 import React from "react"
 
 export default function ShowModal() {
-  
+  return (
+    <!-- Trigger the modal with a button -->
+    <button
+        type="button"
+        className="btn btn-info btn-lg"
+        data-toggle="modal"
+        data-target="#myModal">Open Large Modal</button>
+
+    <!-- Modal -->
+    <div className="modal fade" id="myModal" role="dialog">
+        <div className="modal-dialog modal-lg">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <button type="button" className="close" data-dismiss="modal">&times;</button>
+                    <h4 className="modal-title">Modal Header</h4>
+                </div>
+                <div className="modal-body">
+                    <p>This is a large modal.</p>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
 }
