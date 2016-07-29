a293cbf5eaf57f641b1eceb67dac22573af6cc00
Christopher Wiles <chriswiles87@gmail.com>
2016-07-29 09:31:46 -0500

    fix error msg

--- client/components/NavLogin.js
+++ client/components/NavLogin.js
@@ -14,13 +14,13 @@ export default function NavLogin() {
                 <a href="#" className="btn btn-tw"><i className="fa fa-twitter"></i> Twitter</a>
               </div>
                               or
-               <form className="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+               <form className="form" role="form" method="post" action="login" acceptCharset="UTF-8" id="login-nav">
                   <div className="form-group">
-                     <label className="sr-only" for="exampleInputEmail2">Email address</label>
+                     <label className="sr-only" >Email address</label>
                      <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Email address" required/>
                   </div>
                   <div className="form-group">
-                     <label className="sr-only" for="exampleInputPassword2">Password</label>
+                     <label className="sr-only" >Password</label>
                      <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Password" required/>
                                            <div className="help-block text-right"><a href="">Forget the password ?</a></div>
                   </div>
