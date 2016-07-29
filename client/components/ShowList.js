db6fe856e41cec674b0364eb31d07b1ce49c1e1e
Christopher Wiles <chriswiles87@gmail.com>
2016-07-29 11:54:32 -0500

    rework ShowList.js

---
+++ client/components/ShowList.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react"
+import {connect} from "react-redux"
+import Show from "./Show"
+
+export default class ShowList extends Component {
+
+  _createShows() {
+    const shows = this.props.shows[0];
+    if(shows) {
+      return shows.resultsPage.results.event.map(show => {
+        return <Show
+                key={show.id}
+                displayName={show.displayName}
+                venu={show.venue.displayName}
+                startDate={show.start.date}
+                city={show.location.city}
+                />
+      })
+
+    } else {
+      return <h1>loading</h1>
+    }
+  }
+
+  render() {
+    return (
+      <div className="list-group">
+        {this._createShows()}
+      </div>
+    )
+  }
+}
