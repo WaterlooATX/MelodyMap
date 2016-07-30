96de31020c5522fb13c8e4b5153b4fdec09b7418
Christopher Wiles <chriswiles87@gmail.com>
2016-07-30 12:52:51 -0500

    add Spotify api to server

--- server/models/m_spotifyApi.js
+++ server/models/m_spotifyApi.js
@@ -6,3 +6,13 @@ var spotifyApi = new SpotifyWebApi({
   clientId : 'c7364a23c3714de1882fded9f4142b18',
   clientSecret : 'abdbe447833643d5b85616f5691e2142'
 });
+
+// Search artists whose name contains 'Love'
+exports.searchArtists = (name) => {
+  return spotifyApi.searchArtists(name)
+    .then(function(data) {
+        return data.body.artists.items
+    }, function(err) {
+      console.error(err);
+    });
+}
