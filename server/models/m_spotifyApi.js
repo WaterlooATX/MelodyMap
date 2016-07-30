9105da02673eec42782c80af5ad1b1b31622c969
Christopher Wiles <chriswiles87@gmail.com>
2016-07-30 12:27:17 -0500

    create spotify API model

---
+++ server/models/m_spotifyApi.js
@@ -0,0 +1,8 @@
+// https://github.com/thelinmichael/spotify-web-api-node
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId : 'c7364a23c3714de1882fded9f4142b18',
+  clientSecret : 'abdbe447833643d5b85616f5691e2142'
+});
