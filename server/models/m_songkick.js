5cca3d0fe2c1aa87d3ff0428a8c246e2449e805c
Christopher Wiles <chriswiles87@gmail.com>
2016-08-01 15:43:40 -0500

    add coments for songkick

--- server/models/m_songkick.js
+++ server/models/m_songkick.js
@@ -5,11 +5,12 @@ var client = new Songkick('J4PGT8rVCHdtcfcG') //API KEY
 
 var store = [];
 
+// When specifying min_date or max_date, you need to use both parameters.
+// Use the same value for both to get events for a single day.
+// This search returns only upcoming events.
 exports.getTonightLocalInfo = (coords) => {
 	// Search based on a songkick metro area id
 	// austin 'geo:30.2669444,-97.7431'
 	// `geo:${coords.lat},${coords.long}`
-	return client.searchEvents({
-         "location": `geo:${coords.lat},${coords.long}`
-     }).then((data) => data)
+	return client.searchEvents({"location": `geo:${coords.lat},${coords.long}`}).then((data) => data)
 }
