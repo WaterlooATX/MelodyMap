e613ac7f693dc31aab2b3ec4a6f23b4baedc8245
Christopher Wiles <chriswiles87@gmail.com>
2016-08-02 11:35:00 -0500

    create geolocationAPI

--- client/models/api.js
+++ client/models/api.js
@@ -7,3 +7,11 @@ export function artistInfoAPI(name) {
 export function fetchShowsAPI(long, lat) {
   return axios.post("/fetchShows", {long: long , lat: lat})
 }
+
+export function geolocationAPI(success, fail, options) {
+  return navigator.geolocation.getCurrentPosition(success, fail, options)
+}
+
+export function ipLocationAPI() {
+  return axios('http://ip-api.com/json')
+}
