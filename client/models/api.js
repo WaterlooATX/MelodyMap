d022428b9a6e61dcd1641ec81b4700e780222ba8
Christopher Wiles <chriswiles87@gmail.com>
2016-08-02 11:22:30 -0500

    create fetchShowsAPI

--- client/models/api.js
+++ client/models/api.js
@@ -1,1 +1,9 @@
-//export default 
+import axios from 'axios'
+
+export const artistInfo = () => {
+
+}
+
+export function fetchShowsAPI(geo) {
+  return axios.post("/fetchShows", {long: geo.long , lat: geo.lat})
+}
