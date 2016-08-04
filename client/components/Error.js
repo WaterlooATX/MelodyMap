5104c6ce43a986dadd9d8456feccdeed6b532069
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-08-04 15:27:35 -0500

    tweak some dumb components

--- client/components/Error.js
+++ client/components/Error.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-
-export default class Login extends React.Component{
-	render(){
-			var {errorMsg}=this.props.params;
-		return(
-			<div className="error">
-				<h2> AN ERROR OCCURED</h2>
-				<p>{errorMsg}</p>
-			</div>
-		)
-	}
+const Login = (props) => {
+	var {errorMsg}=props.params;
+	return(
+		<div className="error">
+			<h2> AN ERROR OCCURED</h2>
+			<p>{errorMsg}</p>
+		</div>
+	)
 };
+
+export default Login;
