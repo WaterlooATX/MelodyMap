48845c957b3d070ff86b0156be12d0e36df76157
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-08-04 16:04:00 -0500

    Clean up componenets

--- client/components/UserLogin.js
+++ client/components/UserLogin.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
 
-export default class UserLogin extends React.Component {
-	render(){
-	  return (
-	    <div className='profInfo'>
+const UserLogin = (props) => {
+  return (
+    <div className='profInfo'>
 	    <ul>
-	    	<img className = 'profPic' src={this.props.spotifyData.image} height='55px' width ='55px' />
-	    	<button className='spotify' >
-	    		{console.log(this.props.spotifyData.username)}
-	        	<i className='fa fa-spotify fa-2x'></i><div className='aLink'> {this.props.spotifyData.username.split(' ')[0]} </div>
-	    		
-	      	</button>
+	      <img className = 'profPic' src={props.spotifyData.image} height='55px' width ='55px' />
+	      <button className='spotify' >
+	        { console.log(props.spotifyData.username) }
+          <i className='fa fa-spotify fa-2x'></i><div className='aLink'> { props.spotifyData.username.split(' ')[0] } </div>
+	        </button>
 	    </ul>
-	    </div>
-	  );		
-	}
+    </div>
+  );    
 }
+
+export default UserLogin;
