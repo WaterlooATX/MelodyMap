0c7f04d88540c3fc9a5f1cbc08296992f4743cef
Shane McQuerter <shanemcquerter@gmail.com>
2016-08-04 15:11:02 -0500

    Convert DrawMap to dumb component

--- client/components/DrawMap.js
+++ client/components/DrawMap.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react'
 import { GoogleMapLoader, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import {getDistanceFromLatLonInKm} from "../models/getDistanceFromLatLonInKm"
 
-export default class DrawMap extends Component {
+const DrawMap = (props) => {
 
-  _findClosestShow() {
-    const location = this.props.location;
-    const shows = this.props.shows[0];
+  console.log('props', props);
+  let _Map;
+
+  function _findClosestShow() {
+    const location = props.location;
+    const shows = props.shows[0];
 
     if (shows) {
       let array = shows.map(show => getDistanceFromLatLonInKm(+location.lat, +location.long, +show.venue.lat, +show.venue.lng));
@@ -15,59 +18,59 @@ export default class DrawMap extends Component {
     } else return null;
   }
 
- _setCenter(){
-  const locA = this.props.selectedShow;
-  const locB = this._findClosestShow();
-  const locC = this.props.location;
-  let center;
+  function _setCenter(){
+    const locA = props.selectedShow;
+    const locB = _findClosestShow();
+    const locC = props.location;
+    let center;
 
-  if (locA) center = {lat: +locA.venue.lat, lng: +locA.venue.lng};
-  else if (locB) center = {lat: +locB.venue.lat, lng: +locB.venue.lng};
-  else center = {lat: +locC.lat, lng: +locC.long};
-  return center;
+    if (locA) center = {lat: +locA.venue.lat, lng: +locA.venue.lng};
+    else if (locB) center = {lat: +locB.venue.lat, lng: +locB.venue.lng};
+    else center = {lat: +locC.lat, lng: +locC.long};
+    return center;
   }
 
-  _createMarkers() {
-    return this.props.shows[0].map((show, index) => {
+  function _createMarkers() {
+    return props.shows[0].map((show, index) => {
       return ( <Marker
         key={ index }
         position={ {lat: +show.venue.lat, lng: +show.venue.lng} }
         title ={ show.venue.displayName }
-        onClick={ (marker) => this._onMarkerClickHandler(marker, show) }
+        onClick={ (marker) => _onMarkerClickHandler(marker, show) }
         defaultAnimation= { 2 }
       >
-        {this.props.selectedShow === show ? <InfoWindow content={ show.displayName } /> : null }
+        {props.selectedShow === show ? <InfoWindow content={ show.displayName } /> : null }
       </Marker>
       )
     })
   }
 
-  _onMarkerClickHandler(marker, show) {
-    this._Map.panTo(marker.latLng);
-    this.props.selectShow(show);
-    $(`#heading${this.props.selectedShow.id}`)[0].scrollIntoView( true );
+  function _onMarkerClickHandler(marker, show) {
+    _Map.panTo(marker.latLng);
+    props.selectShow(show);
+    $(`#heading${show.id}`)[0].scrollIntoView( true );
   }
 
-  render() {
-    if (this.props.location.lat) {
-      return (
-        <GoogleMapLoader
-          containerElement={ <div className="mapContainer" /> }
-          googleMapElement={
-            <GoogleMap
-              ref={ (map) => (this._Map = map) }
-              defaultZoom={ 14 }
-              defaultOptions={ {styles: styles, disableDefaultUI: true} }
-              center={ this._setCenter() }
-            >
-              { this.props.shows[0] ? this._createMarkers.call(this) : null }
-            </GoogleMap>
-          }
-        />
-      )
-    } else return null;
-  }
+  if (props.location.lat) {
+    return (
+      <GoogleMapLoader
+        containerElement={ <div className="mapContainer" /> }
+        googleMapElement={
+          <GoogleMap
+            ref={ (map) => (_Map = map) }
+            defaultZoom={ 14 }
+            defaultOptions={ {styles: styles, disableDefaultUI: true} }
+            center={ _setCenter() }
+          >
+            { props.shows[0] ? _createMarkers.call(this) : null }
+          </GoogleMap>
+        }
+      />
+    )
+  } else return null;
 
 }
+
+export default DrawMap;
 
 var styles = [{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#C6E2FF"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#C5E3BF"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#D1D1B8"}]}]
