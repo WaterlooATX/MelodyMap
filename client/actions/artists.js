9b9a94ae7d976b36c5d8dfd8d432e86b74fe217d
Shane McQuerter <shanemcquerter@gmail.com>
2016-08-04 19:05:42 -0500

    Make small tweaks to actions

--- client/actions/artists.js
+++ client/actions/artists.js
@@ -2,6 +2,8 @@ import {fetchShowsAPI} from "../models/api"
 export const FETCH_ARTIST = 'FETCH_ARTIST'
 
 export function fetchArtist(artist) {
+
+
   return {
     type: FETCH_ARTIST,
     payload: fetchShowsAPI(geo.long, geo.lat)
