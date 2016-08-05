6edc499ccae839770569fa24d45dec58372bc53d
Shane McQuerter <shanemcquerter@gmail.com>
2016-08-05 10:12:05 -0500

    Small changes to routes api

--- client/models/api.js
+++ client/models/api.js
@@ -1,45 +1,5 @@
 import axios from 'axios'
 
-export function Songkick_getVenueAPI(venueID) {
-  return axios.post('/Songkick_getVenue', {
-    id: venueID
-  });
-}
-
-export function Spotify_searchArtistsAPI(name) {
-  return axios.post('/Spotify_searchArtists', {
-    name: name
-  });
-}
-
-export function LastFM_getInfoAPI(name) {
-  return axios.post("/LastFM_getInfo", {
-    name: name
-  })
-}
-
-export function Spotify_getArtistTopTracksAPI(artistID, countryCode) {
-  return axios.post('/Spotify_getArtistTopTracks', {
-    id: artistID,
-    code: countryCode
-  });
-}
-
-export function getArtistAlbumsAPI(artistID) {
-  return axios.post('/getArtistAlbums', {
-    id: artistID
-  });
-}
-
-export function Songkick_getSimilarArtistsAPI(artistID) {
-  return axios.post('/Songkick_getSimilarArtists', {
-
-export function getArtistRelatedArtists(artistID) {
-  return axios.post('/getArtistRelatedArtists', {
-    id: artistID
-  });
-}
-
 export function fetchShowsAPI(long, lat) {
   return axios.post("/fetchShows", {
     long: long,
@@ -53,6 +13,43 @@ export function geolocationAPI(success, fail, options) {
 
 export function ipLocationAPI() {
   return axios('http://ip-api.com/json');
+}
+
+export function Spotify_searchArtistsAPI(name) {
+  return axios.post('/Spotify_searchArtists', {
+    name: name
+  });
+}
+
+export function Spotify_getArtistTopTracksAPI(artistID, countryCode) {
+  return axios.post('/Spotify_getArtistTopTracks', {
+    id: artistID,
+    code: countryCode
+  });
+}
+
+export function LastFM_getInfoAPI(name) {
+  return axios.post("/LastFM_getInfo", {
+    name: name
+  })
+}
+
+export function getArtistAlbumsAPI(artistID) {
+  return axios.post('/getArtistAlbums', {
+    id: artistID
+  });
+}
+
+export function Songkick_getVenueAPI(venueID) {
+  return axios.post('/Songkick_getVenue', {
+    id: venueID
+  });
+}
+
+export function Songkick_getSimilarArtistsAPI(artistID) {
+  return axios.post('/Songkick_getSimilarArtists', {
+    id: artistID
+  });
 }
 
 export function Songkick_getEventSetlistAPI(eventID) {
