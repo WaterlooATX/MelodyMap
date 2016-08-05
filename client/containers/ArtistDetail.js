7eff2bfc5ed6bbf32f1a90a1778b6f02e93550c6
Chris Ritchie <chrisritchi@gmail.com>
2016-08-05 16:08:40 -0500

    YouTube Videosss in Artist Detail

--- client/containers/ArtistDetail.js
+++ client/containers/ArtistDetail.js
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
 import NavBar from './NavBar';
-import {Spotify_searchArtistsAPI, Spotify_getArtistTopTracksAPI, getArtistAlbumsAPI, Songkick_getVenueAPI, LastFM_getInfoAPI} from '../models/api';
+import {Spotify_searchArtistsAPI} from '../models/api';
+
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
+import {connect} from "react-redux"
+import {bindActionCreators} from 'redux'
+
+import SearchBar from '../components/SearchBar';
+import VideoList from '../components/VideoList';
+import VideoDetail from '../components/VideoDetail';
+const API_KEY = "AIzaSyAjnuL1a-NSl5B0Kw44-Sd6tgLhQ96R018"
 
 
 export default class ArtistDetail extends Component {
     constructor(props){
     super(props);
     this.state = {
-      artistUri: null
+      artistUri: null,
+      videos: [],
+      selectedVideo: null
     }
   }
 
   componentDidMount() {
     this._spotifyInfo(this.props.params.artistName)
+    this.videoSearch(this.props.params.artistName)
   }
+
 
   render() {
     console.log("STATE", this.state.artistUri)
@@ -23,10 +37,11 @@ export default class ArtistDetail extends Component {
           {/* Display Artist */}
           <h1>
             {`${this.props.params.artistName}`}
+            <iframe className = "followButton" src={`https://embed.spotify.com/follow/1/?uri=${this.state.artistUri}&size=detail&theme=light&show-count=0`}></iframe>
           </h1>
           <h3>
             {`List information for a ${this.props.params.artistName} here. `}
-            This will include:   <iframe src={`https://embed.spotify.com/follow/1/?uri=${this.state.artistUri}&size=detail&theme=light&show-count=0`}></iframe>
+            This will include:   
           </h3>
           <ul>
             <li>Artist Pictures</li>
@@ -38,6 +53,12 @@ export default class ArtistDetail extends Component {
             </li>
             <li>etc</li>
           </ul>
+        <div>
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList
+              onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+              videos={this.state.videos} />
+        </div>
         </div>
       </div>
     )
@@ -51,62 +72,21 @@ _spotifyInfo(artist){
         this.setState({artistUri: obj.data[0].uri})
       })
   }
+
+videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+      })
+    });
+  }
 }
 
-// import React, {Component} from 'react';
-// import NavBar from './NavBar';
-// import React, {Component} from 'react';
-// import NavBar from './NavBar';
-// import _ from 'lodash';
-// import YTSearch from 'youtube-api-search';
-// import {connect} from "react-redux"
-// import {bindActionCreators} from 'redux'
-
-// import SearchBar from '../components/SearchBar';
-// import VideoList from '../components/VideoList';
-// import VideoDetail from '../components/VideoDetail';
 
 
 
-// export default class ArtistDetail extends Component {
 
-//   // componentDidMount() {
-//   //   const query = this.props.location.query;
-//   // }
-
-//   render() {
-//     return (
-//       <div>
-//         <div>
-//           {/* Display Artist */}
-//           <h1>
-//             {`${this.props.location.query.artist}
-//             with spotify artist id of
-//             ${this.props.location.query.spArtistId}`}
-//           </h1>
-//           <h3>
-//             {`List information for a ${this.props.location.query.artist} here. `}
-//             This will include:
-//           </h3>
-//           <ul>
-//             <li>Artist Pictures</li>
-//             <li>Artist Bio</li>
-//             <li>Spofity Song Previews (3)</li>
-//             <li>Upcoming Shows (sortable by both proximity or date)
-//                 This will most likely be the recycled Show/ShowList
-//                 componenets.
-//             </li>
-//             <li>etc</li>
-//           </ul>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-
-
-// // const API_KEY = "AIzaSyAjnuL1a-NSl5B0Kw44-Sd6tgLhQ96R018"
 
 // // export default class Artists extends Component {
 
@@ -114,23 +94,9 @@ _spotifyInfo(artist){
 // //     super(props);
 
 // //     this.state = {
-// //       videos: [],
-// //       selectedVideo: null,
 // //     }
-// //     this.videoSearch('kygo')
-// //   }
-
-// //   videoSearch(term){
-// //     YTSearch({key: API_KEY, term: term}, (videos) => {
-// //       this.setState({
-// //           videos: videos,
-// //           selectedVideo: videos[0]
-// //       })
-// //     });
-// //   }
 
 // //   render() {
-// //     const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
 // //     return (
 
 // //       <div>
@@ -152,13 +118,6 @@ _spotifyInfo(artist){
 // //             <li>Brief intro or bio</li>
 // //             <li>etc</li>
 // //           </ul>
-// //         <div>
-// //           <SearchBar onSearchTermChange={videoSearch}/>
-// //           <VideoDetail video={this.state.selectedVideo} />
-// //           <VideoList
-// //               onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-// //               videos={this.state.videos} />
-// //         </div>
 // //         </div>
 // //       </div>
 // //     )
