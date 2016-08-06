ec12dd9b90cf20d538b51593995b76bef31b21b6
Amanda Fillip <afillip@windstream.net>
2016-08-06 15:32:58 -0500

    added new hompage for artists

---
+++ client/components/ArtistHome.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Artists from '../containers/Artists';
+import SearchBar from './SearchBar';
+
+
+
+class ArtistHome extends Component {
+	render(){
+		<div>
+			<SearchBar/>
+			<Artists/>
+		</div>
+	}
+}
