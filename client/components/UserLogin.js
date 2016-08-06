7462ff8642f6964de848b15698e10a8539acb65d
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-08-06 16:40:17 -0500

    style NavBar

--- client/components/UserLogin.js
+++ client/components/UserLogin.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-
 const UserLogin = (props) => {
   return (
     <div className='profInfo'>
-	    <ul>
-	      <img className = 'profPic' src={props.spotifyData.image} height='55px' width ='55px' />
-	      <button className='spotify' >
-	        { console.log(props.spotifyData.username) }
-          <i className='fa fa-spotify fa-2x'></i><div className='aLink'> { props.spotifyData.username.split(' ')[0] } </div>
-	        </button>
-	    </ul>
+      <img className = 'profPic' src={props.spotifyData.image} height='55px' width ='55px' />
+      <button className='spotify' >
+        <i className='fa fa-spotify fa-2x'></i><div className='aLink'> { props.spotifyData.username.split(' ')[0] } </div>
+      </button>
     </div>
   );    
 }
