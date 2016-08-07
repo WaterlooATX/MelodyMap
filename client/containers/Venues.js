525eda4e9bb4b3871f727ef79143444763d6ec99
Shane McQuerter <shanemcquerter@gmail.com>
2016-08-07 16:34:02 -0500

    Access redux show state for venue info in Venues.js

--- client/containers/Venues.js
+++ client/containers/Venues.js
@@ -9,8 +9,8 @@ import _ from 'lodash';
 class Venues extends Component {
 
   componentDidMount() {
-    this.props.artists ? console.log("typeof this.props.artists in VENUES: ", this.props.artists) : null;
-    this.props.shows ? console.log("typeof this.props.shows in VENUES: ", this.props.shows) : null;
+    this.props.artists ? console.log("this.props.artists in VENUES: ", this.props.artists) : null;
+    this.props.shows ? console.log("this.props.shows in VENUES: ", this.props.shows) : null;
   }
 
   render() {
@@ -57,5 +57,4 @@ export default connect(mapStateToProps)(Venues);
 //   <li>Next show (possibly several)</li>
 //   <li>etc</li>
 // </ul>
-
 
