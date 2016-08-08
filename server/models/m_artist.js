61c1e80ef75211d41086e09ca2dc23142f275e5b
Christopher Wiles <chriswiles87@gmail.com>
2016-08-08 14:42:28 -0500

    working daft m_artist

--- server/models/m_artist.js
+++ server/models/m_artist.js
@@ -1,49 +1,61 @@
-const Songkick = require("./models/m_songkick")
-const Spotify = require("./models/m_spotifyApi")
-const LastFM = require("./models/m_lastFM")
+const Songkick = require("./m_songkick")
+const Spotify = require("./m_spotifyApi")
+const LastFM = require("./m_lastFM")
 
 
 exports.artistInfo = (name) => {
-  Spotify.searchArtists(name)
-    .then(data => callAPIs(data.id))
+  return Spotify.searchArtists(name)
+    .then(data => {
+      let a = data[0]
+      return {
+        spotifyURL: a.external_urls,
+        id: a.id,
+        name: a.name,
+        artistImages: a.images,
+        img: a.images.length ? a.images[1].url : "http://assets.audiomack.com/default-artist-image.jpg",
+        popularity: a.popularity,
+        followers: a.followers.total
+      }
+    })
+    .then(artist => {
+      return Spotify.getArtistRelatedArtists(artist.id)
+        .then(data => {
+          artist['relatedArtists'] = data.artists
+          return artist
+        })
+        .catch(data => artist)
+    })
+    .then(artist => {
+      return Spotify.getArtistAlbums(artist.id)
+        .then(albums => {
+          let a = albums.items.map(album => {
+            return {
+              name: album.name,
+              images: album.images,
+              id: album.id
+            }
+          })
+          artist['albums'] = a
+          return artist
+        })
+        .catch(data => artist)
+    })
+    .then(artist => {
+      return Spotify.getArtistTopTracks(artist.id, countryCode = 'us')
+        .then((data) => {
+          artist['topTracks'] = data.tracks
+          return artist
+        })
+        .catch(data => artist)
+    })
+    .then(artist => {
+      return LastFM.getInfo(artist.name)
+        .then((data) => {
+          artist['summaryBio'] = data.artist.bio.summary
+          artist['fullBio'] = data.artist.bio.content
+          return artist
+        })
+        .catch(data => artist)
+    })
     .catch(error => console.log("error", error))
-}
-
-function callAPIs(spotifyID) {
-  LastFM.getInfo(name)
-    .then((obj) => {
-      res.send(obj)
-    }).catch((error) => {
-      console.log("error", error)
-    })
-
-  Spotify.getArtistRelatedArtists(spotifyID)
-    .then((data) => {
-      res.send(data)
-    }).catch((error) => {
-      console.log("error", error)
-    })
-
-  Songkick.getSimilarArtists(spotifyID)
-    .then((data) => {
-      res.send(data)
-    }).catch((error) => {
-      console.log("error", error)
-    })
-
-
-  Spotify.getArtistAlbums(spotifyID)
-    .then(albums => {
-      res.send(albums)
-    }).catch((error) => {
-      console.log("error", error)
-    })
-
-
-  Spotify.getArtistTopTracks(spotifyID, countryCode)
-    .then((data) => {
-      res.send(data)
-    }).catch((error) => {
-      console.log("error", error)
-    })
 }
