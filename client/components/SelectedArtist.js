be39b7ad4d0eb64a11112e8e50c3e9663f94942c
Amanda Fillip <afillip@windstream.net>
2016-08-08 15:59:23 -0500

    artists and displayed in correct form, image and track wip

--- client/components/SelectedArtist.js
+++ client/components/SelectedArtist.js
@@ -13,36 +13,31 @@ export default class SelectedArtist extends Component{
 	}
 
 	componentDidMount(){
-		console.log("selectedArtist: ", this.props.artists)
+		console.log("selectedArtist: ", this.props.artists);
 	}
 	render(){
 		return (
-
-
 			<div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-				{this.props.artists.map((artist) =>{
-                  <div className="panel panel-default">
-					<div>
-								<ul> artist.displayName</ul>
-								{
-								// <img className="genImage" src = {this.state.img} height='85' width='85'/>
+                <div className="panel panel-default">
+					{this.props.artists.map((artist) =>{
+                  		return (
+                  			<div key={ artist.id } className="panel-heading" role="tab" id={`heading${artist.id}`}>
+                  				<h3>
+									 <img className="genImage" src = {this.state.img} height='85' width='85'/>
 
+									<Link className = "genArtist"
+									    to={ `artist/${artist.displayName}`}
+									    activeClassName='active'>{artist.displayName}
+									</Link>
+								</h3>
 
-								// <Link className = "genArtist"
-								//     to={ `artist/${artist.displayName}`}
-								//     activeClassName='active'>{artist.displayName}
-								// </Link>
-}
-					</div>
-
+                			</div>
+                		)
+					})}
                 </div>
-								})}
               </div>
-
-
 		)
 	}
- 
 
 }
 
