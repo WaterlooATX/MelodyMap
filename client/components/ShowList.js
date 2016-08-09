ee3be44dc43bc6c5d08bfc654a0ecaa9d8f24627
Christopher Wiles <chriswiles87@gmail.com>
2016-08-09 14:09:44 -0500

    Revert "clean up ShowList.js"
    This reverts commit 0d695f44afe1debc9cd22366cf7739623d5f8c8d.

--- client/components/ShowList.js
+++ client/components/ShowList.js
@@ -9,33 +9,19 @@ export default class ShowList extends Component {
   render() {
     const shows = this.props.shows;
     if (shows.length > 0) {
-      if (typeof shows === "string") {
+      if (typeof shows === 'string') return ( <div className="show-error">{ shows }</div> )
+      else {
         return (
-          <div className="show-error">{shows}</div>
-        )
-      } else {
-        return (
-          <div
-            className="panel-group"
-            id="accordion"
-            role="tablist"
-            aria-multiselectable="true">
-            {this._createShows(shows)}
+          <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+              {this._createShows(shows)}
           </div>
         )
       }
     } else {
-      this._spinner();
+      return <div className="spinner">
+      <span className="glyphicon glyphicon-cd" aria-hidden="true"></span>
+      <h1>&nbsp;&nbsp;Fetching your location...</h1></div>
     }
-  }
-
-  _spinner() {
-    return (
-      <div className="spinner">
-        <span className="glyphicon glyphicon-cd" aria-hidden="true"></span>
-        <h1>&nbsp;&nbsp;Fetching your location...</h1>
-      </div>
-    )
   }
 
   // This callback is sent to <Show /> as props to grab show id
@@ -47,7 +33,7 @@ export default class ShowList extends Component {
   }
 
   _createShows(shows) {
-    return shows.map((show, i) => {
+    return shows.map((show, i) => { // 50 shows
       return <Show
         songkick={show}
         ageRestriction={show.ageRestriction}
