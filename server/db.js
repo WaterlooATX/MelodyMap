fe4c06d78eb92f31192ae150468e364351758fbb
Christopher Wiles <chriswiles87@gmail.com>
2016-08-09 14:52:48 -0500

    setup P-Mongo db

--- server/db.js
+++ server/db.js
@@ -1,17 +1,21 @@
 var pmongo = require("promised-mongo");
 var mongodbUri = 'mongodb://MelodyMap:makersquare@ds147995.mlab.com:47995/melodymap'
 
-if (process.env.NODE_ENV === "production") {
-  var db = pmongo(process.env.MONGODB_URI, {
-    authMechanism: "ScramSHA1"
-  });
-} else {
-  var db = pmongo("goodnewsdb");
-}
+// if (process.env.NODE_ENV === "production") {
+//   var db = pmongo(process.env.MONGODB_URI, {
+//     authMechanism: "ScramSHA1"
+//   });
+// } else {
+//   var db = pmongo("goodnewsdb");
+// }
+
+var db = pmongo(mongodbUri, {
+  authMechanism: 'ScramSHA1'
+});
 
 module.exports = db;
 
-// clears database
-db.deleteEverything = function () {
-  return Promise.all([db.collection("articles").remove({})])
-};
+// // clears database
+// db.deleteEverything = function () {
+//   return Promise.all([db.collection("articles").remove({})])
+// };
