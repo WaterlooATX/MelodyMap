c5156404b50c89f1564a7e5904acbf8060a01450
Christopher Wiles <chriswiles87@gmail.com>
2016-08-09 15:32:31 -0500

    artist info

--- server/models/m_artist.js
+++ server/models/m_artist.js
@@ -16,63 +16,96 @@ const db = require("../db")
 //  - update data if old
 
 // Artist.addArtist().then()
+// exports.artistInfo = (name) => {
+//   return Spotify.searchArtists(name)
+//     .then(data => {
+//       if (data) {
+//         let a = data[0]
+//         if (a) {
+//           return {
+//             spotifyURL: a.external_urls,
+//             id: a.id,
+//             songKickName: name,
+//             spotifyName: a.name,
+//             artistImages: a.images,
+//             img: a.images.length ? a.images[1].url : "http://assets.audiomack.com/default-artist-image.jpg",
+//             popularity: a.popularity,
+//             followers: a.followers.total,
+//           }
+//         }
+//       }
+//     })
+// }
 exports.artistInfo = (name) => {
-  const Artist = db.artist
-  console.log(Artist)
-    // Check db(spotifyName)
-    // Artist.findOne({'songKickName': name}, function(err, artist) {
-    //   if (err) return console.log(err);
-    //   console.log(artist)
-    // })
+  return Spotify.searchArtists(name)
+    .then(data => {
+      return data.map(a => {
+        return ({
+          spotifyURL: a.external_urls,
+          id: a.id,
+          songKickName: name,
+          spotifyName: a.name,
+          artistImages: a.images,
+          img: a.images.length ? a.images[1].url : "http://assets.audiomack.com/default-artist-image.jpg",
+          popularity: a.popularity,
+          followers: a.followers.total
+        })
+      })
+    })
 }
 
-Spotify.searchArtists(name)
-  .then(data => {
-    return {
-      spotifyURL: a.external_urls,
-      id: a.id,
-      songKickName: name,
-      spotifyName: a.name,
-      artistImages: a.images,
-      img: a.images.length ? a.images[1].url : "http://assets.audiomack.com/default-artist-image.jpg",
-      popularity: a.popularity,
-      followers: a.followers.total
-    }
-  })
-
-Spotify.getArtistRelatedArtists(artist.id)
-  .then(artist => {
-    return {
-      relatedArtists: data.artists
-    }
-  })
-
-  Spotify.getArtistAlbums(artist.id)
-    .then(albums => {
-      return {
-        albums: albums.items.map(album => {
-          return {
-            name: album.name,
-            images: album.images,
-            id: album.id
-          }
-        })
-      }
-    })
-
-Spotify.getArtistTopTracks(artist.id, countryCode = "us")
-  .then(artist => {
-    return {
-      topTracks: artist.tracks
-    }
-  })
-
-LastFM.getInfo(name)
-  .then(data => {
-    return {
-      summaryBio: data.artist.bio.summary,
-      fullBio: data.artist.bio.content
-    }
-  })
+// Spotify.searchArtists(name)
+//   .then(data => {
+//     if (data) {
+//       let a = data[0]
+//       if (a) {
+//         return {
+//           spotifyURL: a.external_urls,
+//           id: a.id,
+//           songKickName: name,
+//           spotifyName: a.name,
+//           artistImages: a.images,
+//           img: a.images.length ? a.images[1].url : "http://assets.audiomack.com/default-artist-image.jpg",
+//           popularity: a.popularity,
+//           followers: a.followers.total,
+//         }
+//       }
+//     }
+//   })
+//
+// Spotify.getArtistRelatedArtists(artist.id)
+//   .then(artist => {
+//     return {
+//       relatedArtists: data.artists
+//     }
+//   })
+//
+//   Spotify.getArtistAlbums(artist.id)
+//     .then(albums => {
+//       return {
+//         albums: albums.items.map(album => {
+//           return {
+//             name: album.name,
+//             images: album.images,
+//             id: album.id
+//           }
+//         })
+//       }
+//     })
+//
+// Spotify.getArtistTopTracks(artist.id, countryCode = "us")
+//   .then(artist => {
+//     return {
+//       topTracks: artist.tracks
+//     }
+//   })
+//
+// LastFM.getInfo(name)
+//   .then(data => {
+//     return {
+//       summaryBio: data.artist.bio.summary,
+//       fullBio: data.artist.bio.content
+//     }
+//   })
 
 //.catch(error => console.log("error", error))
