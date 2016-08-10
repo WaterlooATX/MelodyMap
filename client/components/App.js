d1d1f47ba90b7b9227e803297f1f8363fe260f65
Christopher Wiles <chriswiles87@gmail.com>
2016-08-10 12:30:20 -0500

    remove div

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,13 @@
 import React, {Component} from "react"
-import NavBar from '../containers/NavBar'
-
+import NavBar from "../containers/NavBar"
 
 const App = (props) => {
   return (
     <div>
-      <NavBar />
+      <NavBar/>
       <div className="temp">
         <div className="temp">
-          { props.children }
+          {props.children}
         </div>
       </div>
     </div>
