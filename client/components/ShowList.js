e8c0a640538d99b418cc752807bfb312b9225657
Christopher Wiles <chriswiles87@gmail.com>
2016-08-10 12:18:26 -0500

    clean up ShowList

--- client/components/ShowList.js
+++ client/components/ShowList.js
@@ -9,19 +9,33 @@ export default class ShowList extends Component {
   render() {
     const shows = this.props.shows;
     if (shows.length > 0) {
-      if (typeof shows === 'string') return ( <div className="show-error">{ shows }</div> )
-      else {
+      if (typeof shows === "string") {
         return (
-          <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-              {this._createShows(shows)}
+          <div className="show-error">{shows}</div>
+        )
+      } else {
+        return (
+          <div
+            className="panel-group"
+            id="accordion"
+            role="tablist"
+            aria-multiselectable="true">
+            {this._createShows(shows)}
           </div>
         )
       }
     } else {
-      return <div className="spinner">
-      <span className="glyphicon glyphicon-cd" aria-hidden="true"></span>
-      <h1>&nbsp;&nbsp;Fetching your location...</h1></div>
+      return this._spinner()
     }
+  }
+
+  _spinner() {
+    return (
+      <div className="spinner">
+        <span className="glyphicon glyphicon-cd" aria-hidden="true"></span>
+        <h1>&nbsp;&nbsp;Fetching your location...</h1>
+      </div>
+    )
   }
 
   // This callback is sent to <Show /> as props to grab show id
@@ -33,7 +47,7 @@ export default class ShowList extends Component {
   }
 
   _createShows(shows) {
-    return shows.map((show, i) => { // 50 shows
+    return shows.map(show => {
       return <Show
         songkick={show}
         ageRestriction={show.ageRestriction}
@@ -48,7 +62,7 @@ export default class ShowList extends Component {
         startDate={show.start.date}
         venue={show.venue.displayName}
         venueID={show.venue.id}
-        onNavigateClick= { this.props.onNavigateClick.bind(this) }
+        onNavigateClick={this.props.onNavigateClick.bind(this)}
       />
     })
   }
