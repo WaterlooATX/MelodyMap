1a7c13ae121dec954565bc80a200eaf61e6e9bd4
Christopher Wiles <chriswiles87@gmail.com>
2016-08-10 12:27:47 -0500

    fix

--- client/containers/NavBar.js
+++ client/containers/NavBar.js
@@ -31,6 +31,7 @@ class NavBar extends Component {
   // grab url, send accessToken/refreshToken to actions
     const url = document.location.href.split("/")
     const self = this;
+    if (url[5]) {
     //Spotify call to follow artist followArtist(url[5],'3TNt4aUIxgfy9aoaft5Jj2')
       this.setState({loggedIn: true});
       this.props.setTokens(url[5], url[6]);
