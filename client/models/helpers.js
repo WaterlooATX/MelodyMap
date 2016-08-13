fc0de642b94a118d6831ab21712ad6d5a0302fe1
Christopher Wiles <chriswiles87@gmail.com>
2016-08-13 15:39:45 -0500

    add new helpers

--- client/models/helpers.js
+++ client/models/helpers.js
@@ -6,3 +6,23 @@ export const getAlbumArt = (Artist) => {
   albumArt = albumArt ? albumArt.images[1].url : 'http://assets.audiomack.com/default-album-image.jpg'
   return albumArt
 }
+export const getBio = (Artist) => {
+  const randomBio = "The music sails alive with the compelling combination of rich layers among mixed styles of rhythm that hit the soul. By melding hook-filled melody within hard and heavy beats, has the ability to compact a vast array of influence and experience into a singular song"
+
+  const checkBio = (fullBio) => {
+    if(fullBio && fullBio.length) {
+      return fullBio.slice(0,225).split('/').join(' /').split('%').join('% ').split('<a')[0] + '...'
+    } else {
+      return randomBio
+    }
+  }
+
+  return Artist ? checkBio(Artist.fullBio) : randomBio
+}
+export const isReduxLoaded = (obj) => {
+  if(Object.keys(obj).length) {
+    return true
+  } else {
+    return false
+  }
+}
