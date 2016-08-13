2e74e47c0bbf6777e66c6abe1d7c7387c3424777
Christopher Wiles <chriswiles87@gmail.com>
2016-08-13 15:13:50 -0500

    clean up

--- client/components/DropdownArtistTitle.js
+++ client/components/DropdownArtistTitle.js
@@ -7,22 +7,26 @@ export default class DropdownArtistsTitle extends Component {
     return this.props.venue ? this._renderVenue() : null
   }
 
+  _doorsOpen() {
+    return !this.props.doorsOpen.includes('Invalid date') ? `Doors open ${this.props.doorsOpen}` : null
+  }
+
   _renderVenue() {
     return (
       <div className="panel-top">
         <div className="marker" onClick={ this.props.onNavigateClick.bind(this) }>
-          <i id="marker" className="fa fa-map-marker fa-4" aria-hidden="true"></i>
+          <i id="marker" className="fa fa-map-marker fa-4" aria-hidden="true"/>
         </div>
         <div className="left">
           <div id="venueName">
             {/* Route to VenueDetails page on click of venue */}
-            { <Link to={`/venue/${this.props.venue.name}/${this.props.venue.id}`} activeClassName='active'>{this.props.venue.name}</Link> }
+            <Link to={`/venue/${this.props.venue.name}/${this.props.venue.id}`} activeClassName='active'>{this.props.venue.name}</Link>
             <div id="venueAdress">
               { this.props.venue.address }
             </div>
-              <div id="doorsOpen">
-                { !this.props.doorsOpen.includes('Invalid date') ? `Doors open ${this.props.doorsOpen}` : null }
-              </div>
+            <div id="doorsOpen">
+              {this._doorsOpen()}
+            </div>
           </div>
         </div>
       </div>
