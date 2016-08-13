cb15302c9456a9a4d0ed81401dde38b813ab9914
Christopher Wiles <chriswiles87@gmail.com>
2016-08-13 11:31:09 -0500

    rework ArtistList

--- client/components/ArtistList.js
+++ client/components/ArtistList.js
@@ -1,51 +1,47 @@
 import React, {Component} from 'react'
+import ArtistItem from '../components/ArtistItem'
 
 export default class ArtistList extends Component {
-  constructor(props){
+
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       songPlayed: false,
       songButton: null
     }
+  }
+
+  _songPlayToggle(songPlayed, songButton) {
+    this.setState({
+      songPlayed,
+      songButton
+    })
+  }
+
+  _createArtists() {
+    const artists = this.props.artists
+    const mapped = []
+    for (let artist in artists) {
+      mapped.push(
+        <ArtistItem
+          artist={artists[artist]}
+          key={artists[artist].id}
+          name={artists[artist].name}
+          songPlayed={ this.state.songPlayed }
+          songButton={ this.state.songButton }
+          songPlayToggle={ this._songPlayToggle.bind(this) }
+        />
+      )
+    }
+    return mapped
   }
 
   render() {
     const Artists = this._createArtists()
     return (
       <div>
-        asd
+        {Artists}
       </div>
-
     )
-  }
-
-  _songPlayToggle(songPlayed, songButton) {
-    this.setState({ songPlayed, songButton })
-  }
-
-  _createArtists() {
-    const artists = this.props.artists
-    for(let artist in artists){
-
-      // fetchArtistsAPI(artist).then((Artist)=>{
-      //   // console.log(Artist)
-      //   artists[artist]['onTourUntil'] = Artist.data[0].onTourUntil
-      // })
-      console.log(artist)
-    }
-  //   const mapped = []
-  //   for (let artist in artists) {
-  //     mapped.push(
-  //       <ArtistItem
-  //         artist={artists[artist]}
-  //         key={artist}
-  //         name={artist}
-  //         songPlayed={ this.state.songPlayed }
-  //         songButton={ this.state.songButton }
-  //         songPlayToggle={ this._songPlayToggle.bind(this) }
-  //       />
-  //     )
-  //   }
-  //   return mapped
   }
 }
