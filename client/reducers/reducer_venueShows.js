6139802c406cc5898606c3b965f3e5d468dfbb4c
Shane McQuerter <shanemcquerter@gmail.com>
2016-08-12 19:48:54 -0500

    Delete old VenueShows redux files

--- client/reducers/reducer_venueShows.js
+++
@@ -1,9 +0,0 @@
-// import { VENUE_SHOWS } from "../actions/venue_shows";
-
-// export default function(state = {}, action) {
-//   switch (action.type) {
-//     case VENUE_SHOWS:
-//     return action.payload
-//   }
-//   return state
-// }
