99302c48cbc665674f896927a0987f78ca4ac1c9
Christopher Wiles <chriswiles87@gmail.com>
2016-08-13 14:32:13 -0500

    moved to own file

---
+++ client/components/DropdownArtistTitle.js
@@ -0,0 +1,31 @@
+import {Link} from 'react-router'
+import React, {Component} from 'react'
+
+export default class DropdownArtistsTitle extends Component {
+
+  render() {
+    return this.props.venue ? this._renderVenue() : null
+  }
+
+  _renderVenue() {
+    return (
+      <div className="panel-top">
+        <div className="marker" onClick={ this.props.onNavigateClick.bind(this) }>
+          <i id="marker" className="fa fa-map-marker fa-4" aria-hidden="true"></i>
+        </div>
+        <div className="left">
+          <div id="venueName">
+            {/* Route to VenueDetails page on click of venue */}
+            { <Link to={`/venue/${this.props.venue.name}/${this.props.venue.id}`} activeClassName='active'>{this.props.venue.name}</Link> }
+            <div id="venueAdress">
+              { this.props.venue.address }
+            </div>
+              <div id="doorsOpen">
+                { !this.props.doorsOpen.includes('Invalid date') ? `Doors open ${this.props.doorsOpen}` : null }
+              </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
