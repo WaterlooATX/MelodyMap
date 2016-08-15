982a45580b90b8a263898aa7d022c8aee547b410
Christopher Wiles <chriswiles87@gmail.com>
2016-08-15 12:41:45 -0500

    formating

--- client/containers/ArtistDetail.js
+++ client/containers/ArtistDetail.js
@@ -7,6 +7,7 @@ import {connect} from "react-redux";
 import {bindActionCreators} from 'redux'
 import {Link} from 'react-router';
 import UpcomingShows from '../components/UpcomingShows'
+import {getAlbumArt, topTrack, getBio} from '../models/helpers'
 import VideoList from '../components/VideoList';
 import VideoDetail from '../components/VideoDetail';
 const API_KEY = "AIzaSyAjnuL1a-NSl5B0Kw44-Sd6tgLhQ96R018"
@@ -23,14 +24,14 @@ export default class ArtistDetail extends Component {
       artistBlocks: []
     }
   }
+
   componentDidMount() {
-    this.videoSearch(this.props.params.artistName)
-    this.getArtist(this.props.params.artistName)
+    this._videoSearch(this.props.params.artistName)
+    this._getArtist(this.props.params.artistName)
   }
 
   componentWillReceiveProps() {
-    this.videoSearch(this.props.params.artistName)
-    this.filterArtist(this.props.params.artistName)
+    this._videoSearch(this.props.params.artistName)
   }
 
 
@@ -41,28 +42,29 @@ export default class ArtistDetail extends Component {
             <div className="jumbotron">
                 <img className = "detailImage img-circle" src = {this.state.artistImg}/>
                 <h1>{`${this.props.params.artistName}`}</h1>
-                <h3>{this.onTour(this.state.artistTour)}</h3>
+                <h3>{this._onTour(this.state.artistTour)}</h3>
                 <ul>
-                {this.getGenre(this.state.artistGenre)}
+                {this._getGenre(this.state.artistGenre)}
                 </ul>
+                <iframe src={`https://embed.spotify.com/follow/1/?uri=spotify:artist:${this.state.artistID}&size=basic&theme=light&show-count=0`} width="200" height="25" scrolling="no" frameBorder="0" allowTransparency="true"></iframe>
+
                 {this.state.artistBio !== "" ? <div>{this.state.artistBio}  <div id="bio" className="collapse">{this.state.bio}</div></div> :
                 <p> The music sails alive with the compelling combination of rich
                 layers among mixed styles of rhythm that hit the soul.
                 By melding hook-filled melody within hard and heavy beats,
                 has the ability to compact a vast array of influence and experience
                 into a singular song</p>}
+
                 <button type="button" className="btn btn-info" data-toggle="collapse" data-target="#bio">Show More</button>
             </div>
           </div>
         {this.state.artistShows ? <div className = "upcoming-shows"> <h3>Upcoming Shows</h3>
-        <div className="scrollable-menu">{this.getShows(this.state.artistShows)} </div></div>: null}
+        <div className="scrollable-menu">{this._getShows(this.state.artistShows)} </div></div>: null}
         <div className="media-container">
           <VideoDetail video={this.state.selectedVideo} />
-          <iframe src={`https://embed.spotify.com/?uri=spotify:trackset:TopTracks:${this.getTopTracks(this.state.artistTopTracks)}`} width="370px" height= "510px" frameBorder="0" allowTransparency="true"></iframe>
         </div>
           <div className="container-similar">
-            <h3> Similar Artists </h3>
-            <p className="text-muted credit">{this.similarArtists(this.state.artistSimilar)}</p>
+            <p className="text-muted credit">{this._similarArtists(this.state.artistSimilar)}</p>
           </div>
       </div>
     )
@@ -70,41 +72,27 @@ export default class ArtistDetail extends Component {
 
 
 
-videoSearch(term){
-    YTSearch({key: API_KEY, term: term}, (videos) => {
+  _videoSearch(term) {
+    YTSearch({
+      key: API_KEY,
+      term: term
+    }, (videos) => {
       this.setState({
-          videos: videos,
-          selectedVideo: videos[0]
+        videos: videos,
+        selectedVideo: videos[0]
       })
     })
   }
 
-  shortenBio(bio){
-    for(var i = 0;i<bio.length;i++){
-      if(bio[i] === "<"){
-        var newBio = bio.slice(0,i)
-        if(newBio.length > 400){
-          var bioSnippit = newBio.slice(0,400)
-          var bioFull = newBio.slice(400,bio.length)
-          this.setState({bio: bioFull})
-          return bioSnippit + "..."
-        }
-        else{
-          return newBio
-        }
-      }
-    }
-  }
-
-  onTour(tour){
+  _onTour(tour){
     if(tour === "1"){
       return <div className = "text-muted">ON TOUR NOW!</div>
-    }
-    else{
+    }else{
       return null
     }
   }
-  getGenre(genres){
+
+  _getGenre(genres){
     if(!genres){
       return null
     }
@@ -114,98 +102,96 @@ videoSearch(term){
       })
     }
   }
-  similarArtists(artists){
-    if(!artists){
-      return null
-    }
-    else{
-      return artists.map(artist => {
-        return artist.image.map(image => {
-          if(image.size === "large"){
-            if(image["#text"] === ""){
-              return <div className='similar-artist'>
-                <img className ="img-circle" src = "http://assets.audiomack.com/default-artist-image.jpg" />
-                <Link className ="genArtist"
-                        to={ `/artist/${artist.name}`}
-                                activeClassName='active'>{artist.name}
 
-                </Link>
-                </div>
-            }
-            else{
-            return <div className="similar-artist">
-              <img className = "img-circle" src = {image["#text"]}/>
-              <Link className = "genArtist"
-                        to={ `/artist/${artist.name}`}
-                        activeClassName='active'>{artist.name}
+  _similarArtists(artists) {
+      if (!artists) {
+          return null
+      } else {
+          return artists.map(artist => {
+              return artist.image.map(image => {
+                  if (image.size === "large") {
+                      if (image["#text"] === "") {
+                          return <div className="similar-artist">
+                              <img
+                                  className="img-circle"
+                                  src="http://assets.audiomack.com/default-artist-image.jpg"/>
+                              <Link
+                                  className="genArtist"
+                                  to={`/artist/${artist.name}`}
+                                  activeClassName="active">{artist.name}
 
-              </Link>
-            </div>
-          }
-          }
-         else{
-          return null;
-         }
-        })
-      })
-    }
-  }
+                              </Link>
+                          </div>
+                      } else {
+                          return <div className="similar-artist">
+                              <img className="img-circle" src={image["#text"]}/>
+                              <Link
+                                  className="genArtist"
+                                  to={`/artist/${artist.name}`}
+                                  activeClassName="active">{artist.name}
 
-  getTopTracks(tracks){
-      if(!tracks){
-        return null;
+                              </Link>
+                          </div>
+                      }
+                  } else {
+                      return null;
+                  }
+              })
+          })
       }
-      else{
-      return tracks.map(track => {
-        return track.id
-      })
-    }
   }
 
-  getShows(shows){
-    if(!shows){
+  _getTopTracks(tracks) {
+    if (!tracks) {
       return null;
-    }
-    else{
-      return shows.data.map(show => {
-        console.log("SHOWSSS",show)
-        return (<UpcomingShows show={show} key={show.id} source="VenueDetail"/>)
-      })
+    } else {
+      return tracks.map(track => track.id)
     }
   }
-  getArtist(artist){
-    let artistsArr = []
-      fetchArtistsAPI(artist).then(data => {
-        var mapped = data.data.map(artistData => {
-        return { name:artistData.displayName, id:artistData.id }
-        })
-      Songkick_getArtistCalendarAPI(mapped[0].id).then(shows =>{
-        this.setState({artistShows: shows})
-      })  
-      mapped.forEach((artist)=>{
-      Spotify_searchArtistsAPI(artist).then((spotify)=>{
-        if(spotify.data){
-          this.setState({
-        artistBio: this.shortenBio(spotify.data.fullBio),
-        artistName: spotify.data.name,
-        artistImg: spotify.data.img,
-        artistID: spotify.data.id,
-        artistGenre: spotify.data.genre,
-        artistTopTracks: spotify.data.topTracks,
-        artistTour: spotify.data.onTour,
-        artistSimilar: spotify.data.relatedArtists[0].artist
-      })
 
-          artistsArr.push(spotify.data)
-          this.setState({artistBlocks: artistsArr})
+  _getShows(shows) {
+      if (!shows) {
+          return null;
+      } else {
+          return shows.data.map(show => {
+              return <UpcomingShows show={show} key={show.id} source="VenueDetail"/>
+          })
+      }
+  }
+
+  _getArtist(artist) {
+    let artistsArr = []
+    fetchArtistsAPI(artist).then(data => {
+      var mapped = data.data.map(artistData => {
+        return {
+          name: artistData.displayName,
+          id: artistData.id
         }
       })
-     })
+      mapped.forEach((artist) => {
+        Spotify_searchArtistsAPI(artist).then((spotify) => {
+          if (spotify.data) {
+            this.setState({
+              artistBio: getBio(spotify.data.fullBio),
+              artistName: spotify.data.name,
+              artistImg: spotify.data.img,
+              artistID: spotify.data.id,
+              artistGenre: spotify.data.genre,
+              artistTopTracks: spotify.data.topTracks,
+              artistTour: spotify.data.onTour,
+              artistSimilar: spotify.data.relatedArtists[0].artist
+            })
+
+            artistsArr.push(spotify.data)
+            this.setState({
+              artistBlocks: artistsArr
+            })
+          }
+        })
+      })
     })
   }
-
 }
 
 const mapStateToProps = (state) => {return {artists: state.artists}};
 export default connect(mapStateToProps)(ArtistDetail);
-
