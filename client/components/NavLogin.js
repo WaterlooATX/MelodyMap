2cb5694829d01808a953eb5458a8e4fd69492c47
Shane McQuerter <shanemcquerter@gmail.com>
2016-08-15 10:47:57 -0500

    Create venues.css and make spotify button clickable

--- client/components/NavLogin.js
+++ client/components/NavLogin.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NavLogin = (props) => {
   return (
     <div className="NavLogin">
-      <button type='button' className='spotify'>
+      <button type='button' className='spotify' onClick={ function() { window.location.href='/login' } }>
         <i className='fa fa-spotify fa-2x'></i><a className='aLink' href="/login">Login with Spotify</a>
       </button>
     </div>
