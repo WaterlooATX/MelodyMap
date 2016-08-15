6b8b48d6d9ef7a3310f06c97c1e4dd1889891315
Shane McQuerter <shanemcquerter@gmail.com>
2016-08-15 17:28:25 -0500

    Approximate marker pin/venue styling

--- client/components/GenVenue.js
+++ client/components/GenVenue.js
@@ -16,17 +16,37 @@ export default class GenVenue extends Component {
               activeClassName='active'>{venue.name}
           </Link>
         </h1>
-          <div>
-            <h4>
-              <span id="marker"><i className="fa fa-map-marker fa-3" aria-hidden="true"></i></span>
-              <a className = "venueAddress"
-                   href={`http://maps.google.com/?q=${venue.address}`} target="_blank">{venue.address}
-              </a>
-            </h4>
-          </div>
+        <h4>
+          <a className = "venueAddress"
+             href={`http://maps.google.com/?q=${venue.address}`} target='_blank'>{venue.address}
+             <i id="marker" className="fa fa-map-marker fa-4" aria-hidden="true"></i>
+          </a>
+        </h4>
       </div>
     )
   }
 
 }
+
+
+
+// export default class GenVenue extends Component {
+
+//   render() {
+//     let venue = this.props.venue
+
+//     return (
+//       <div className="genvenue-venue" id={`heading${venue.id}`}>
+//         <h1>
+//           <Link className = "genvenue-name-link"
+//               to={ `/venue/${venue.name}/${venue.id}`}
+//               activeClassName='active'>{venue.name}
+//           </Link>
+//         </h1>
+//         <h4>{venue.address}</h4>
+//       </div>
+//     )
+//   }
+
+// }
 
