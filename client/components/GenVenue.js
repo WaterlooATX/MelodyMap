a3dbc733a8da55dc8f9b214c16f9c0a1fd4ac325
Shane McQuerter <shanemcquerter@gmail.com>
2016-08-15 19:09:47 -0500

    Finish styling of marker/address in venues.js

--- client/components/GenVenue.js
+++ client/components/GenVenue.js
@@ -16,37 +16,20 @@ export default class GenVenue extends Component {
               activeClassName='active'>{venue.name}
           </Link>
         </h1>
-        <h4>
-          <a className = "venueAddress"
-             href={`http://maps.google.com/?q=${venue.address}`} target='_blank'>{venue.address}
-             <i id="marker" className="fa fa-map-marker fa-4" aria-hidden="true"></i>
-          </a>
-        </h4>
-      </div>
+
+<div className="genvenue-address">
+          <h4>
+            <a className = "venueAddress"
+               href={`http://maps.google.com/?q=${venue.address}`} target='_blank'>
+                 <i id="marker" className="fa fa-map-marker fa-4" aria-hidden="true"></i>
+                 {venue.address}
+            </a>
+          </h4>
+</div>
+</div>
     )
   }
 
 }
 
-
-
-// export default class GenVenue extends Component {
-
-//   render() {
-//     let venue = this.props.venue
-
-//     return (
-//       <div className="genvenue-venue" id={`heading${venue.id}`}>
-//         <h1>
-//           <Link className = "genvenue-name-link"
-//               to={ `/venue/${venue.name}/${venue.id}`}
-//               activeClassName='active'>{venue.name}
-//           </Link>
-//         </h1>
-//         <h4>{venue.address}</h4>
-//       </div>
-//     )
-//   }
-
-// }
 
