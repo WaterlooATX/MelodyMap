e6dd0fc0c042234aeab08b408fd58f3c8e85bf1a
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-08-18 16:42:23 -0500

    style ArtistDetail media boxes

--- client/components/VideoDetail.js
+++ client/components/VideoDetail.js
@@ -13,9 +13,9 @@ const VideoDetail = ({video}) => {
   const url = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return (
-    <div className="video-detail col-md-8">
+    <div className="video-detail">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" src={url}/>
+        <iframe style={{ width: '100%' }} className="embed-responsive-item" src={url}/>
       </div>
     </div>
   )
