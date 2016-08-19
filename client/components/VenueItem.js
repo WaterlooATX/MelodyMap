fe483940dc867808ae62cfc0e1618aaa949913ed
Christopher Wiles <chriswiles87@gmail.com>
2016-08-19 15:12:41 -0500

    format

--- client/components/VenueItem.js
+++ client/components/VenueItem.js
@@ -1,37 +1,29 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router';
 
-
 export default class VenueItem extends Component {
 
   render() {
     const venue = this.props.venue
-    const image = venue.photo ? venue.photo : '/assets/venue-image.jpg'
+    const image = venue.photo ? venue.photo : "/assets/venue-image.jpg"
     return (
       <div className="col-md-4 VenueItem" id={`heading${venue.id}`}>
-		        <Link className = "venueLink"
-		           	  id='venueLink'
-		              to={ `/venue/${venue.name}/${venue.id}`}
-		              activeClassName='active'>
-			     	<img
-		              className="genImage"
-		              src={image}
-		              height="105"
-		              width="105"
-	            	/>
-                <div className="venueNameAndMaker">
-                  {/* <div id="venueMarker" href={`http://maps.google.com/?q=${venue.address}`} target='_blank'>
-                     <i id="marker" className="fa fa-map-marker fa-4" aria-hidden="true"></i>
-                  </div> */}
-                  <div className="text-center venueName">{venue.name}</div>
-                </div>
-
-		        </Link>
-
-                  <div className='text-center venueStreetAddress'>
-                      {venue.city + ', ' + venue.state}
-                  </div>
-	  </div>
+        <Link
+          className="venueLink"
+          id="venueLink"
+          to={`/venue/${venue.name}/${venue.id}`}
+          activeClassName="active">
+          <img className="genImage" src={image} height="105" width="105"/>
+          <div className="venueNameAndMaker">
+            <div className="text-center venueName">
+              {venue.name}
+            </div>
+          </div>
+        </Link>
+        <div className="text-center venueStreetAddress">
+          {venue.city + ", " + venue.state}
+        </div>
+      </div>
     )
   }
 }
