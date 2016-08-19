9471665bc23aa05b4db3be9e71fcde2cc9aa17f0
Christopher Wiles <chriswiles87@gmail.com>
2016-08-19 07:56:34 -0500

    refactor

--- client/components/AudioPlayer.js
+++ client/components/AudioPlayer.js
@@ -7,26 +7,32 @@ export default class AudioPlayer extends Component {
     return (
       <div className = "audioPlayer-container">
 				<div className ="audioPlayer-name list-group">
-				Top Tracks By {this.props.artist.name}
-				{this._getTopTracks(this.props.artist.topTracks)}
+				    {this._checkTracks(this.props.artist.topTracks)}
 				</div>
 			</div>
-
     )
   }
-  _getTopTracks(tracks) {
-    if (!tracks) {
-      return null;
-    } else {
-      return tracks.map(track => {
-        return (
-          <div className="audioPlayer-list-item list-group-item" key={track.id}>{track.name}
-          	<div className="audioPlayer-speaker" key={track.id}>
-              <Speaker track ={track.preview_url} key={track.id} size = {2}/>
-            </div>
-        	</div>
-        )
-      })
-    }
+
+  _checkTracks(tracks) {
+    return tracks ? this._tracks(tracks) : null
+  }
+
+  _tracks(tracks) {
+    return tracks.map(track => <Tracks track={track} key={track.id}/>)
+  }
+
+}
+
+class Tracks extends Component {
+  render() {
+    const track = this.props.track
+    return (
+      <div className="audioPlayer-list-item list-group-item">
+        {track.name}
+        <div className="audioPlayer-speaker">
+          <Speaker track ={track.preview_url} key={track.id} size = {2}/>
+        </div>
+      </div>
+    )
   }
 }
