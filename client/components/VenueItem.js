1b8524600288d60fc40df93cef47060a11b4c684
Amanda Fillip <afillip@windstream.net>
2016-08-19 12:48:11 -0500

    default image displayed

--- client/components/VenueItem.js
+++ client/components/VenueItem.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router';
 
+
 export default class VenueItem extends Component {
 
   render() {
     const venue = this.props.venue
-    const image = venue.photo ? venue.photo : 'https://ivacationinyourhell.files.wordpress.com/2012/12/night-shift.jpg'
+    const image = venue.photo ? venue.photo : '/assets/defaultImage.jpg'
+    console.log(image)
     return (
       <div className="col-md-4 gridding" id={`heading${venue.id}`}>
 	        <div className ="artist-label" id='selected'>
