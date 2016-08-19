5cbd66920ffe2a3abb08557a3e4bd7afcddbf0a9
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-08-19 11:31:08 -0500

    style ArtistDetail

--- client/components/AudioPlayer.js
+++ client/components/AudioPlayer.js
@@ -30,7 +30,7 @@ class Tracks extends Component {
       <div className="audioPlayer-list-item list-group-item">
         <div className="audioPlayer-trackName">{track.name.slice(0,30)}</div>
         <div className="audioPlayer-speaker">
-          <Speaker track ={track.preview_url} key={track.id} size = {2}/>
+          <Speaker track ={track.preview_url} key={track.id} size = {1}/>
         </div>
       </div>
     )
